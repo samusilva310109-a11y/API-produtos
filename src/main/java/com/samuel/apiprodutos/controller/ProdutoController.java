3687a96757a17bd321df6f5afd8d216f83de6c3a
samusilva310109-a11y <samusilva310109@gmail.com>
2026-07-02 16:07:20 -0300

    Adicionado anotações sobre as Spring Annotations. Criado nova rota para filtragem de produtos por nome.

--- src/main/java/com/samuel/apiprodutos/controller/ProdutoController.java
+++ src/main/java/com/samuel/apiprodutos/controller/ProdutoController.java
@@ -32,6 +32,11 @@ public class ProdutoController {
     public List<Produto> listarProdutos(){
         return produtoRepository.findAll();
     }
+
+    @GetMapping("/")
+    public  List<Produto> buscarPorNome(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
+    }
     
     @GetMapping("/{id}")
     public Produto buscarProduto(@PathVariable("id") String id){
@@ -52,4 +57,26 @@ public class ProdutoController {
         return produtoRepository.save(produtoAtualizado);
     }
 
+    /*
+    * Spring Annotations:
+    *
+    * Métodos HTTP:
+    *   @GetMapping() -> Define uma requisição do tipo GET
+    *   @PostMapping() -> Define uma requisição do tipo POST
+    *   @DeleteMapping() -> Define uma requisição do tipo DELETE
+    *   @PutMapping() -> Define uma requisição do tipo PUT
+    *
+    *   @RequestBody -> Define que os dados enviados no corpo da requisição em JSON devem serem convertidos em formato Obejeto Java
+    *   @PathVariable("<variável>") -> Define uma variável params no Endpoint. Tem como parâmetro o nome da variável em específico.
+    *   @RequestParam("<query_parms>") -> Define uma variável query params no Endpoint. Tem como parâmetro o nome da variável em específico.
+    *
+    *   @RequestMapping() -> Utilizada em classes para estabelecer uma rota base para os métodos da classe.
+    *                       Define a rota final e o método HTTP
+    *
+    *   @RestController -> Define uma classe como camada de Controller de API RestFul
+    *   @Entity -> Define uma classe com uma entidade no banco de dados
+    *   @Table("<nome_tabela>") -> Define a tabela vinculada a uma classe
+    *   @Column() -> Define uma variável como uma coluna na tabela
+    *   @Id() -> Define uma variável como identificador da tabela
+    */
 }
--- src/main/java/com/samuel/apiprodutos/repository/ProdutoRepository.java
+++ src/main/java/com/samuel/apiprodutos/repository/ProdutoRepository.java
@@ -3,5 +3,9 @@ package com.samuel.apiprodutos.repository;
 import com.samuel.apiprodutos.model.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
 }
